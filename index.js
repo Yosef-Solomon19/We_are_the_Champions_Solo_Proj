0e6ae36f0230312b276bbf5dc378daba7e53dc29
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2024-01-03 13:41:20 -0500

    Adding func to clear input when clicked

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const publishBtnEl = document.querySelector("#btn-el")
 function test() {
    console.log("clicked")
 }
+
 publishBtnEl.addEventListener("click", function(){
     test()
     console.log(textAreaEL.value)
+    clearInputField(textAreaEL)
 })
+
+function clearInputField(input) {
+    input.value =""
+}
