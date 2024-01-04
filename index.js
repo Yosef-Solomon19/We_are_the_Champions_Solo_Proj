0910578428ad0e76d38b8a4dc9d53ff1874c053e
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2024-01-04 11:25:23 -0500

    Initializing and connecting to firebase project.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 'use strict';
+// Import to initialize app, get database, etc
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const textAreaEL = document.querySelector("#textarea-el")
 const publishBtnEl = document.querySelector("#btn-el")
 
-function test() {
-   console.log("clicked")
+const appSettings = {
+    databaseURL: "https://endorsement-app-78f29-default-rtdb.firebaseio.com/"
 }
+// Create dictionary to store database reference URL
+// This is to connect our app to the database project 
+const app = initializeApp(appSettings)
+
+// Get the databse from the firebase project 
+const database = getDatabase(app)
+
+// Create reference, a location inside the database to store our data (endorsements)
+const endorsementinDB = ref(database, "endorsements")
 
 publishBtnEl.addEventListener("click", function(){
     test()
@@ -13,6 +25,10 @@ publishBtnEl.addEventListener("click", function(){
     clearInputField(textAreaEL)
 })
 
-function clearInputField(input) {
-    input.value =""
+function test() {
+   console.log("clicked")
+}
+
+function clearInputField(inputEl) {
+    inputEl.value =""
 }
